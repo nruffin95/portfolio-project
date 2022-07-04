7604d6492096704060e8e17bc22a2a298a75350d
nruffin95 <nruffin95@gmail.com>
2022-07-04 11:18:56 -0500

    Updat with Employee Project

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,10 @@
 const datePage = document.getElementById('datePage')
+const eap = document.getElementById("eap");
 
 datePage.addEventListener('click', function(){
     window.open("https://phaberle.github.io/Hookup/index.html");
+})
+
+eap.addEventListener('click', function(){
+    window.open("https://test-keith97-app.herokuapp.com/");
 })
